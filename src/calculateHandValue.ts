43095bd61f7d6c8afc953b1032f4a1376e6e983a
Forrest Wolf <forrest.weiswolf@gmail.com>
2024-09-01 12:11:03 -0400

    Move calculateHandValue to own file

---
+++ src/calculateHandValue.ts
@@ -0,0 +1,46 @@
+const calculateHandValue = (cards: any[]) => {
+  let total = 0;
+  cards.forEach((card: any) => {
+    if (card.hidden === false && card.value !== 'A') {
+      switch (card.value) {
+        case 'K':
+          total += 10;
+          break;
+        case 'Q':
+          total += 10;
+          break;
+        case 'J':
+          total += 10;
+          break;
+        default:
+          total += Number(card.value);
+          break;
+      }
+    }
+  });
+  const aces = cards.filter((card: any) => {
+    return card.value === 'A';
+  });
+  aces.forEach((card: any) => {
+    if (card.hidden === false) {
+      if ((total + 11) > 21) {
+        total += 1;
+      }
+      else if ((total + 11) === 21) {
+        if (aces.length > 1) {
+          total += 1;
+        }
+        else {
+          total += 11;
+        }
+      }
+      else {
+        total += 11;
+      }
+    }
+  });
+
+  return total;
+}
+
+export default calculateHandValue
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import Status from './Status';
 import Controls from './Controls';
 import Hand from './Hand';
 import standardDeck from '../deck.js';
+import calculateHandValue from '../calculateHandValue';
 
 const App: React.FC = () => {
   enum GameState {
@@ -180,47 +181,7 @@ const App: React.FC = () => {
   }
 
   const calculate = (cards: any[], setScore: any) => {
-    let total = 0;
-    cards.forEach((card: any) => {
-      if (card.hidden === false && card.value !== 'A') {
-        switch (card.value) {
-          case 'K':
-            total += 10;
-            break;
-          case 'Q':
-            total += 10;
-            break;
-          case 'J':
-            total += 10;
-            break;
-          default:
-            total += Number(card.value);
-            break;
-        }
-      }
-    });
-    const aces = cards.filter((card: any) => {
-      return card.value === 'A';
-    });
-    aces.forEach((card: any) => {
-      if (card.hidden === false) {
-        if ((total + 11) > 21) {
-          total += 1;
-        }
-        else if ((total + 11) === 21) {
-          if (aces.length > 1) {
-            total += 1;
-          }
-          else {
-            total += 11;
-          }
-        }
-        else {
-          total += 11;
-        }
-      }
-    });
-    setScore(total);
+    setScore(calculateHandValue(cards));
   }
 
   const hit = () => {
