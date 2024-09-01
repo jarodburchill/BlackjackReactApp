e517c56da097494a38753d619e656d02d8fd221f
Forrest Wolf <forrest.weiswolf@gmail.com>
2024-09-01 12:16:20 -0400

    Move enums out of component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,29 +5,29 @@ import Hand from './Hand';
 import standardDeck from '../deck.js';
 import calculateHandValue from '../calculateHandValue';
 
+enum GameState {
+  bet,
+  init,
+  userTurn,
+  dealerTurn
+}
+
+enum Deal {
+  user,
+  dealer,
+  hidden
+}
+
+enum Message {
+  bet = 'Place a Bet!',
+  hitStand = 'Hit or Stand?',
+  bust = 'Bust!',
+  userWin = 'You Win!',
+  dealerWin = 'Dealer Wins!',
+  tie = 'Tie!'
+}
+
 const App: React.FC = () => {
-  enum GameState {
-    bet,
-    init,
-    userTurn,
-    dealerTurn
-  }
-
-  enum Deal {
-    user,
-    dealer,
-    hidden
-  }
-
-  enum Message {
-    bet = 'Place a Bet!',
-    hitStand = 'Hit or Stand?',
-    bust = 'Bust!',
-    userWin = 'You Win!',
-    dealerWin = 'Dealer Wins!',
-    tie = 'Tie!'
-  }
-
   const [deck, setDeck]: any[] = useState(standardDeck);
 
   const [userCards, setUserCards]: any[] = useState([]);
