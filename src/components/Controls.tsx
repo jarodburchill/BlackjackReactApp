97751122f901091a28ff7f4def4d77d31206e219
Forrest Wolf <forrest.weiswolf@gmail.com>
2024-09-01 18:06:59 -0400

    Simplify return value of App

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -32,44 +32,34 @@ const Controls: React.FC<ControlsProps> = ({ balance, gameState, buttonState, be
     return true;
   }
 
-  const amountChange = (e: any) => {
-    setAmount(e.target.value);
-  }
-
   const onBetClick = () => {
     if (validation()) {
       betEvent(Math.round(amount * 100) / 100);
     }
   }
 
-  const getControls = () => {
-    if (gameState === 0) {
-      return (
-        <div className={styles.controlsContainer}>
-          <div className={styles.betContainer}>
-            <h4>Amount:</h4>
-            <input autoFocus type='number' value={amount} onChange={amountChange} className={inputStyle} />
-          </div>
-          <button onClick={() => onBetClick()} className={styles.button}>Bet</button>
+  if (gameState === 0) {
+    return (
+      <div className={styles.controlsContainer}>
+        <div className={styles.betContainer}>
+          <h4>Amount:</h4>
+          <input
+            autoFocus type='number' value={amount}
+            onChange={(e) => setAmount(Number(e.target.value))} className={inputStyle}
+          />
         </div>
-      );
-    }
-    else {
-      return (
-        <div className={styles.controlsContainer}>
-          <button onClick={() => hitEvent()} disabled={buttonState.hitDisabled} className={styles.button}>Hit</button>
-          <button onClick={() => standEvent()} disabled={buttonState.standDisabled} className={styles.button}>Stand</button>
-          <button onClick={() => resetEvent()} disabled={buttonState.resetDisabled} className={styles.button}>Reset</button>
-        </div>
-      );
-    }
+        <button onClick={() => onBetClick()} className={styles.button}>Bet</button>
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.controlsContainer}>
+        <button onClick={() => hitEvent()} disabled={buttonState.hitDisabled} className={styles.button}>Hit</button>
+        <button onClick={() => standEvent()} disabled={buttonState.standDisabled} className={styles.button}>Stand</button>
+        <button onClick={() => resetEvent()} disabled={buttonState.resetDisabled} className={styles.button}>Reset</button>
+      </div>
+    );
   }
-
-  return (
-    <>
-      {getControls()}
-    </>
-  );
 }
 
 export default Controls;
